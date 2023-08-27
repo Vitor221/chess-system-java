ed3dab3e8cc3268b2c8d9f35e229ca06a5642c4f
Vitor <vitornascimento6650@gmail.com>
2023-08-27 00:10:40 -0300

    Bispo

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,5 +1,6 @@
 package Chess;
 
+import Chess.pieces.Bishop;
 import Chess.pieces.King;
 import Chess.pieces.Pawn;
 import Chess.pieces.Rook;
@@ -188,8 +189,11 @@ public class ChessMatch {
 
     // Colocando as peças no tabuleiro
     private void initialSetup() {
+
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -201,7 +205,9 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
